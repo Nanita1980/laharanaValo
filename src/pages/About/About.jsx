a3895caaa8fff2a04dbbd464eb1977432d43f76b
EricaRaz <erica_raza@yahoo.fr>
2023-09-20 11:07:53 +0200

    mise en place de Header dans la page About

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const About = () => {
+
+
+  return (
+    <div>
+      <div className="containerAbout">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default About;
