20eb2c55393c75a81f9877de3d563f6e4238bccb
EricaRaz <erica_raza@yahoo.fr>
2023-09-21 17:05:49 +0200

    mise en place de ErrorPage

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 import About from '../../pages/About/About';
+import AccomSheet from '../AccomSheet/AccomSheet';
+import ErrorPage from '../../pages/ErrorPage/ErrorPage'
 
 const Root = () => {
   return (
@@ -8,7 +10,8 @@ const Root = () => {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
- 
+              <Route path="/logement/:id" element={<AccomSheet />} />
+              <Route path="*" element={<ErrorPage />} />
           </Routes>
       </BrowserRouter>
   );
