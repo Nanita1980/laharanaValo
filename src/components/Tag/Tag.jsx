c3783747a3a8c3a1bdabbf627f163a752e531407
EricaRaz <erica_raza@yahoo.fr>
2023-09-25 18:19:40 +0200

    mise en place des title, location, tag dans Accommodation

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tag = ({ tag }) => {
+  return (
+    <div className="displayTag">
+      <p>{tag}</p>
+    </div>
+  );
+};
+
+export default Tag;
--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -2,9 +2,11 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import db from "../../data/db.json";
 import Carousel from "../../components/Carousel/Carousel";
+import Tag from "../../components/Tag/Tag";
 
 const Accommodation = ({ id }) => {
   const housing = db.find((housing) => housing.id === id);
+  const tags = housing.tags;
 
   return (
     <>
@@ -12,6 +14,20 @@ const Accommodation = ({ id }) => {
       <Header />
         <div className="containerHouse">
           <Carousel pictures={housing.pictures} />
+
+          <div className="infos">
+            <div className="location">
+              <h2 className="housingTitle">{housing.title}</h2>
+              <p className="place">{housing.location}</p>
+
+              <div className="tags">
+                {tags.map((tag, index) => (
+                  <Tag key={index} tag={tag} />
+                ))}
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
       <Footer />
