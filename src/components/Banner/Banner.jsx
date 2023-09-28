e067703198fe49d6914a3bbc37de229431cd779b
EricaRaz <erica_raza@yahoo.fr>
2023-09-28 13:33:34 +0200

    changement de nom wherePage

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,20 +3,20 @@ import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function Banner() {
-  const [aboutPage, setAboutPage] = useState(false);
-
+  const [wherePage, setWherePage] = useState(false);
+  
   const location = useLocation();
 
   useEffect(() => {
     if (location.pathname === "/about") {
-      setAboutPage(true);
+      setWherePage(true);
     }
   }, [location.pathname]);
 
   return (
     <div className="banner">
-      <section className={aboutPage ? "bannerAbout" : "bannerHome"}>
-        {!aboutPage && <h1 className="bannerText" >Chez vous, partout et ailleurs</h1>}
+      <section className={wherePage ? "bannerAbout" : "bannerHome"}>
+        {!wherePage && <h1 className="bannerText" >Chez vous, partout et ailleurs</h1>}
       </section>
     </div>
   );
